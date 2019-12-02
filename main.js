adb80476846abaae3438b3e6a7f7c14d77e62177
Reza Shoja <rezasj88@gmail.com>
2019-12-02 20:50:09 +0330

    Finished Project

---
+++ main.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+  $(".flash").click(function() {
+    $(".brain").toggleClass("laser");
+  });
+
+  $(".color").click(function() {
+    var red = Math.floor(Math.random() * 255);
+    var green = Math.floor(Math.random() * 255);
+    var blue = Math.floor(Math.random() * 255);
+
+    var randomRGBA = "rgba(" + red + "," + green + "," + blue + ", 1)";
+
+    $("body").css("background", randomRGBA);
+  });
+
+  $(".moves").click(function() {
+    $("img").toggle();
+  });
+});
